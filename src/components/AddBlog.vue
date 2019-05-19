9b482d021b791c1eef5c036486cf033d0ed9a272
ManBaCao <2323332693@qq.com>
2019-05-19 20:31:55 +0800

    添加vue工程

---
+++ src/components/AddBlog.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="add-blog">
+        <h2>添加博客</h2>
+        <form v-if="!submmited">
+            <label>博客标题</label>
+            <input type="text" v-model="blog.title" required/>
+            <label>博客内容</label>
+            <textarea v-model="blog.content"></textarea>
+
+            <div id="checkBoxes">
+                <label>Vue.js</label>
+                <input type="checkbox" value="Vue.js" v-model="blog.categories">
+                <label>Node.js</label>
+                <input type="checkbox" value="Node.js" v-model="blog.categories">
+                <label>React.js</label>
+                <input type="checkbox" value="React.js" v-model="blog.categories">
+                <label>Angular.js</label>
+                <input type="checkbox" value="Angular.js" v-model="blog.categories">
+            </div>
+                <label>作者:</label>
+                <select v-model="blog.author">
+                    <option v-for="(author,index) in authors" :key="index">
+                        {{author}}
+                    </option>
+                </select>
+                <button v-on:click.prevent="post">添加博客</button>
+        </form>
+
+        <div v-if="submmited">
+            <h3>您的博客发布成功!</h3>
+        </div>
+
+        <div id="preview">
+            <h3>博客总览</h3>
+            <p>博客标题: {{blog.title}}</p>
+            <p>博客内容:</p>
+            <p>{{blog.content}}</p>
+            <p>博客分类</p>
+            <ul>
+                <li v-for="(category,index) in blog.categories" :key="index">
+                    {{category}}
+                </li>
+            </ul>
+            <p>作者:{{blog.author}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'add-blog',
+    data () {
+        return {
+            blog: {
+                title: "",
+                content: "",
+                categories: [],
+                author: "",
+            },
+            authors: ["Vinter", "Bryant", "Jordan"],
+            submmited: false,
+        }
+    },
+    methods: {
+        post: function () {
+            this.$http.post("http://jsonplaceholder.typicode.com/posts",{
+                title: this.blog.title,
+                body: this.blog.content,
+                userId: 1,
+            })
+            .then(function (data) {
+                this.submmited = true;
+                console.log(data);
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
